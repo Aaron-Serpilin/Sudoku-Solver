97441d8bb86be06d6a0db01964d5e1065bfd5776
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-08-14 16:59:04 +0200

    Clarified Eliminate Function and Comments
    The Eliminate function has been slightly clarified, and comments throughout the file have been placed to make the code easier to follow

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,12 @@
+#Throughout the project there are repeated functions or lines of code. They are different versions of the same thing. They are there since they are easier to use for debugging/readability/comprehension. Final version will only have the simplified versions
+
 rows = 'ABCDEFGHI'
 columns = '123456789'
-first_grid_string = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+first_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 
 def grid_unit_creator (row, column): #Concatenates all possible combinations of the given parameters
     return [i + j for i in row for j in column]
 
-#Same outcome as the function above except easier to understand 
 # def grid_unit_creator (row, column): 
 #     grid_array = []
 #     for i in row:
@@ -17,12 +18,11 @@ boxes = grid_unit_creator(rows, columns) #Returns all the boxes of the sudoku gr
 row_units = [grid_unit_creator(row, columns) for row in rows] #Returns all the rows of the sudoku grid
 col_units = [grid_unit_creator(rows, column) for column in columns] #Returns all the columns of the sudoku grid
 square_units = [grid_unit_creator(square_row, square_column) for square_row in ('ABC', 'DEF', 'GHI') for square_column in ('123', '456', '789')] #Returns all the boxes of the sudoku grid
-unit_list = row_units + col_units + square_units
+unit_list = row_units + col_units + square_units  #Contains every row, column, and 3x3 square to apply the only choice strategy
 units = dict((s, [u for u in unit_list if s in u]) for s in boxes) #Maps the row, column, and 3x3 square that every box belongs to
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes) #Maps all the peers of every box
 
 def grid_values(grid):
-
     values = []
     all_digits = '123456789'
 
@@ -37,21 +37,37 @@ def grid_values(grid):
     grid_dictionary = dict(zip(boxes, values))
     return grid_dictionary
 
-def eliminate(values):
-    solved_values = [box for box in values.keys() if len(values[box]) == 1] #Returns the boxes that have solved values based on the prompted grid
-    #print(f'Here are the values {values}\n Here are the value keys {values.keys()}')
-    for box in solved_values:
-        digit = values[box] #Digit equals the values of the solved boxes
-        for peer in peers[box]:
-            values[peer] = values[peer].replace(digit,'')
-    return values
+def eliminate(grid):
+    grid_values = grid.keys()
+    solved_values = [box for box in grid_values if len(grid[box]) == 1] #Returns the boxes that have solved values based on the prompted grid
+
+    # solved_values = []
+    # for box in grid_values:
+    #     if len(grid[box]) == 1:
+    #         solved_values.append(box)
+
+    #print(f'The whole grid is\n {grid}\nThe solved values are:\n {solved_values}')
+
+    for box in solved_values: #We loop through every box in the grid
+        digit = grid[box] #Digit equals the values of the solved boxes
+        peers_of_solved_boxes = peers[box] #Equals the peers of the solved box
+
+        for peer in peers_of_solved_boxes: #We systematically loop through all the peers of the solved boxes, and if the digit of the solved box is in the digit string of one of its peers, it is removed. This is done repeatedly
+            grid[peer] = grid[peer].replace(digit,'')
+    
+    return grid #We return a grid that has only solved boxes and a smaller list of possible numbers for the unsolved boxes
 
 def only_choice(values): #This function goes over every box and sees if there is a unique only choice for a box, then it assigns it
+
+    all_digits = '123456789'
+
     for unit in unit_list:
-        for digit in '123456789':
+        
+        for digit in all_digits:
             possible_digits = [box for box in unit if digit in values[box]]
             if len(possible_digits) == 1:
                 values[possible_digits[0]] = digit
+
     return values
 
 def reduce_puzzle(values):
@@ -72,8 +88,8 @@ def reduce_puzzle(values):
             return False
     return values
 
-eliminate(grid_values(first_grid_string))
-#print(reduce_puzzle(grid_values(first_grid_string)))
+#eliminate(grid_values(first_grid))
+#print(reduce_puzzle(grid_values(first_grid)))
 
 
 
