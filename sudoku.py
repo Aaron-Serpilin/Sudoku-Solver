123f667c595cbc69b2ec0150d3028aa94f8b4218
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-08-14 18:32:49 +0200

    Vanilla Sudoku Solver Done
    The sudoku solver can solve a limited number of sudoku grids. Corresponding comments have been added

--- sudoku.py
+++ sudoku.py
@@ -2,7 +2,7 @@
 
 rows = 'ABCDEFGHI'
 columns = '123456789'
-first_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+first_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'        
 
 def grid_unit_creator (row, column): #Concatenates all possible combinations of the given parameters
     return [i + j for i in row for j in column]
@@ -57,23 +57,22 @@ def eliminate(grid):
     
     return grid #We return a grid that has only solved boxes and a smaller list of possible numbers for the unsolved boxes
 
-def only_choice(values): #This function goes over every box and sees if there is a unique only choice for a box, then it assigns it
+def only_choice(values): #This function loops through every unit, either a row, column, or 3x3 square, and sees that if there is a digit that only appears once, hence, the only choice, to assign the box that value
 
     all_digits = '123456789'
-
-    for unit in unit_list:
-        
-        for digit in all_digits:
-            possible_digits = [box for box in unit if digit in values[box]]
-            if len(possible_digits) == 1:
-                values[possible_digits[0]] = digit
+    
+    for unit in unit_list: 
+        for digit in all_digits: 
+            boxes_with_digit = [box for box in unit if digit in values[box]] #This variable stores the peer boxes that contain the certain digit 
+            if len(boxes_with_digit) == 1: #If only one box from the peer boxes contains the certain digit, it means it is the only possible place where it can fit, it is the only choice. 
+                values[boxes_with_digit[0]] = digit
 
     return values
 
 def reduce_puzzle(values):
     stalled = False
     while not stalled:
-        # Check how many boxes have a determined value
+        # Check how many boxes have a determined value, meaning they have been solved
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
         # Use the Eliminate Strategy
         values = eliminate(values)
@@ -81,7 +80,7 @@ def reduce_puzzle(values):
         values = only_choice(values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
-        # If no new values were added, stop the loop.
+        # If no new values were added, stop the loop. This means we cannot further reduce the sudoku grid through merely these two constraints
         stalled = solved_values_before == solved_values_after
         # Sanity check, return False if there is a box with zero available values:
         if len([box for box in values.keys() if len(values[box]) == 0]):
@@ -89,9 +88,6 @@ def reduce_puzzle(values):
     return values
 
 #eliminate(grid_values(first_grid))
-#print(reduce_puzzle(grid_values(first_grid)))
-
-
-
-
+#only_choice(eliminate(grid_values(first_grid)))
+print(reduce_puzzle(only_choice(eliminate(grid_values(first_grid)))))
 
