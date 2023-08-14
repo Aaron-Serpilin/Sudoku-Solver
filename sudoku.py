3ad89ce2ecba9d7199df6009fe9f9476fe15adb6
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-08-14 19:19:27 +0200

    Functional Sudoku Solver using Depth First Search
    Fully functional solver. Need to polish code

--- sudoku.py
+++ sudoku.py
@@ -2,7 +2,8 @@
 
 rows = 'ABCDEFGHI'
 columns = '123456789'
-first_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'        
+first_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'   
+second_grid = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'     
 
 def grid_unit_creator (row, column): #Concatenates all possible combinations of the given parameters
     return [i + j for i in row for j in column]
@@ -87,7 +88,24 @@ def reduce_puzzle(values):
             return False
     return values
 
+def search(values):
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
 #eliminate(grid_values(first_grid))
 #only_choice(eliminate(grid_values(first_grid)))
-print(reduce_puzzle(only_choice(eliminate(grid_values(first_grid)))))
-
+#print(reduce_puzzle(only_choice(eliminate(grid_values(first_grid)))))
+print(search(reduce_puzzle(only_choice(eliminate(grid_values(second_grid))))))
